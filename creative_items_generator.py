879e64fca60cdf9fc319f7ccab20f54df5c3a92b
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2022-03-01 14:28:00 +0200

    made block states dynamic

--- creative_items_generator.py
+++ creative_items_generator.py
@@ -1,12 +1,42 @@
 import base64
 from binary_utils.binary_stream import binary_stream
 import json
+from nbt_utils.utils.nbt_net_le_binary_stream import nbt_net_le_binary_stream
 from nbt_utils.utils.nbt_le_binary_stream import nbt_le_binary_stream
+from nbt_utils.tag.compound_tag import compound_tag
+
+class BlockStateMap:
+    def __init__(self, tag: compound_tag):
+        self.runtime_to_state: list = []
+        self.state_to_runtime: dict = {}
+        metadata_counter: int = 0
+        old_name: str = ""
+        for runtime_id, inner_tag in enumerate(tag.value):
+            name: str = inner_tag.get_tag("name").value
+            if name != old_name:
+                metadata_counter = 0
+            self.runtime_to_state.append(f"{name} {metadata_counter}")
+            self.state_to_runtime[f"{name} {metadata_counter}"] = runtime_id
+            metadata_counter += 1
+            old_name = name
+
+    def get_runtime_to_state(self, runtime_id: int) -> tuple:
+        if runtime_id < len(self.runtime_to_state):
+            tmp: tuple = self.runtime_to_state[runtime_id].split(" ")
+            return tmp[0], int(tmp[1])
+        else:
+            return "", 0
+
+    def get_state_to_runtime(self, name: str, metadata: int) -> int:
+        if f"{name} {metadata}" in self.state_to_runtime:
+            return self.state_to_runtime[f"{name} {metadata}"]
+        else:
+            return -1
 
 class ItemStateMap:
-    def __init__(self, states: list[dict[[str, int, bool]]]):
-        self.runtime_to_state: dict[[int, str]] = {}
-        self.state_to_runtime: dict[[str, int]] = {}
+    def __init__(self, states: list):
+        self.runtime_to_state: dict = {}
+        self.state_to_runtime: dict = {}
         for state in states:
             self.runtime_to_state[state["runtime_id"]] = state["name"]
             self.state_to_runtime[state["name"]] = state["runtime_id"]
@@ -23,9 +53,17 @@ class ItemStateMap:
         else:
             return 0
 
+def create_block_state_map() -> BlockStateMap:
+    file = open("block_states.nbt", "rb")
+    stream: nbt_net_le_binary_stream = nbt_net_le_binary_stream(file.read())
+    file.close()
+    tag: compound_tag = compound_tag()
+    tag.read(stream)
+    return BlockStateMap(tag)
+
 def create_item_state_map() -> ItemStateMap:
     file = open("item_states.json", "rt")
-    data: list[dict[[str, int, bool]]] = json.loads(file.read())
+    data: list = json.loads(file.read())
     file.close()
     return ItemStateMap(data)
 
@@ -66,6 +104,7 @@ def create_creative_content() -> list:
     file.close()
     return decode_creative_content_packet(stream)
 
+block_states: BlockStateMap = create_block_state_map()
 item_states: ItemStateMap = create_item_state_map()
 creative_content: list = create_creative_content()
 creative_items: list = []
@@ -78,8 +117,13 @@ for item in creative_content:
         stream: nbt_le_binary_stream = nbt_le_binary_stream()
         stream.write_root_tag(item["nbt"])
         creative_item["nbt_b64"] = base64.b64encode(stream.data).decode()
-    if item["block_runtime_id"] != 0:
-        creative_item["block_runtime_id"] = item["block_runtime_id"]
+    if item["network_id"] < 256:
+        state: tuple = block_states.get_runtime_to_state(item["block_runtime_id"])
+        creative_item["block_state_name"] = state[0]
+        creative_item["block_state_metadata"] = state[1]
+    else:
+        if item["block_runtime_id"] != 0:
+            print(creative_item["name"])
     creative_items.append(creative_item)
 
 file = open("creative_items.json", "wt")
